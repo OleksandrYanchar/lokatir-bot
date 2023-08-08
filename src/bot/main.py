9d047469cb4ca507fd680e4a53165406426e8958
Oleksandr Yanchar <oleksandryancharr@gmail.com>
2023-08-08 21:56:06 +0300

    Init

---
+++ src/bot/main.py
@@ -0,0 +1,84 @@
+import telebot
+import os
+from dotenv import load_dotenv
+from telebot import types
+from settings import questions, answers
+
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+user_data = {}  # Store current question and score for each user
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    start_button = types.KeyboardButton('/quiz')
+    markup.add(start_button)
+
+    bot.send_message(message.chat.id, 'Привіт, я бот-вікторина Локатира романа.', reply_markup=markup)
+
+
+@bot.message_handler(commands=['quiz'])
+def start(message):
+    bot.send_message(message.chat.id, 'Квіз розпочато.')
+    user_data[message.chat.id] = {'question_index': 0, 'score': 0}
+    send_question(message.chat.id)
+
+def send_question(user_id):
+    user_info = user_data.get(user_id, {'question_index': 0, 'score': 0})
+    question_index = user_info['question_index']
+
+    if question_index < len(questions):
+        question = questions[question_index]
+        keyboard = types.InlineKeyboardMarkup()
+        for ans in answers[question]:
+            keyboard.add(types.InlineKeyboardButton(text=ans, callback_data=ans))
+
+        formatted_question = f'<b>{question}</b>'  # Додайте тег <b> для жирного шрифту
+        bot.send_message(user_id, formatted_question, reply_markup=keyboard, parse_mode='HTML')
+    else:
+        send_result_message(user_id, user_info['score'])
+
+def send_result_message(user_id, score):
+    if -1000 < score <= 0:
+        result_message = 'Ви взагалі не знаєте Романа, ідіть підівчіться та не позортесь'
+    elif 0 < score <= 10:
+        result_message = 'Дуже слабенько, ви напевно лише вчора дізналися хто такий Роман'
+        bot.send_photo(user_id, photo=open('../pictures/pidyob.png' ,'rb'))
+    elif 10 < score <= 30:
+        result_message = 'Ви знаєте Романа не перший день, але все одно цього недостатньо'
+        bot.send_photo(user_id, photo=open('../pictures/patriot.png', 'rb'))
+    elif 30 < score <= 70:
+        bot.send_photo(user_id, photo=open('../pictures/ok.png', 'rb'))
+        result_message = 'Непогано, ще трішки і ви зможете сказати, що ви фанат Романа'
+        bot.send_photo(user_id, photo=open('../pictures/dovolniy.png', 'rb'))
+    elif 70 < score <= 130:
+        result_message = 'Ви справжній фанат Романа'
+        bot.send_photo(user_id, photo=open('../pictures/cool_1.png', 'rb'))
+    elif 130 < score <= 140:
+        result_message = 'Ви Локатир Романа, або його кращий друг'
+        bot.send_photo(user_id, photo=open('../pictures/bratva.png', 'rb'))
+    else:
+        result_message = 'Сама ти нікому не потрібна, шмара'
+        bot.send_photo(user_id, photo=open('../pictures/minus.jpg', 'rb'))
+
+    bot.send_message(user_id, f'Ваш рахунок: {score}')
+
+    bot.send_message(user_id, result_message)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def answer(call):
+    answer = call.data
+    user_id = call.message.chat.id
+    user_info = user_data.get(user_id, {'question_index': 0, 'score': 0})
+    question_index = user_info['question_index']
+    question = questions[question_index]
+
+    if answer in answers[question]:
+        user_info['score'] += answers[question][answer]
+        user_info['question_index'] += 1
+        send_question(user_id)
+
+bot.polling()

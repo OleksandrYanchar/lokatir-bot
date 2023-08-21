d84f0e9701ff028f03e457d3528279a2ad646c1d
Oleksandr Yanchar <oleksandryancharr@gmail.com>
2023-08-21 21:56:29 +0300

    ADD:users id adding func

--- src/bot/admin_commands.py
+++ src/bot/admin_commands.py
@@ -4,15 +4,13 @@ from aiogram import Bot, Dispatcher, types
 from settings import bot, dp, chupa_id, chupa_ID, admins_ID,photos_directory
 from users_database import  UsersDatabase
 
-
-
-db_manager = UsersDatabase()
+users_db = UsersDatabase()
 
 @dp.message_handler(commands=['admin'])
 async def admin_menu(message: types.Message):
     if message.from_user.id in admins_ID:
         user_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        user_markup.add('/restartTracking', '/stopTracking', '/changeTrackID').add('/users','/sendAlert','/stats').add('/back')
+        user_markup.add('/restartTracking', '/stopTracking', '/changeTrackID').add('/users','/sendAlert','/stats').add('/addUser','/back')
         await message.reply("Адмін меню", reply_markup=user_markup)
 @dp.message_handler(commands=['back'])
 async def default_menu(message: types.Message):
@@ -25,7 +23,7 @@ async def default_menu(message: types.Message):
 @dp.message_handler(commands=['users'])
 async def get_ids(message: types.Message):
     users = []
-    db_entries = db_manager.get_all_entries()
+    db_entries = users_db.get_all_entries()
     for entry in db_entries:
         users.append(entry[0])
     await bot.send_message(message.chat.id, f"{users} ")
@@ -36,10 +34,13 @@ async def send_message(message: types.Message):
     if message.from_user.id in admins_ID:
         if len(message.text.split())> 1:
             alert_text = message.text.replace('/sendAlert', '').strip()
-            db_entries = db_manager.get_all_entries()
+            db_entries = users_db.get_all_entries()
             for entry in db_entries:
                 user_id = entry[0]
-                await bot.send_message(user_id, alert_text)
+                try:
+                    await bot.send_message(user_id, alert_text)
+                except Exception as e:
+                    await bot.send_message(message.chat.id, f'{e}:{user_id}')
         else:
             await  message.reply('введи текст')
 
@@ -78,6 +79,23 @@ async def change_track_id(message: types.Message):
                 await bot.send_message(IDs, f'тепер ,бот трекає {chupa_id}')
         else:
             await message.reply("Введіть команду у форматі /changeTrackID ID")
+@dp.message_handler(commands=['addUser'])
+async def add_user(message: types.Message):
+    if message.from_user.id in admins_ID:
+        if len(message.text.split()) > 1:
+            user_ids = [id.strip() for id in message.text.split()[1:] if id.strip().isdigit()]
+            added_users = 0
+            for user_id in user_ids:
+                if not users_db.user_exists(user_id):
+                    users_db.add_user(user_id)
+                    added_users += 1
+            if added_users > 0:
+                await message.reply(f'додано {added_users} користувачів до бд')
+            else:
+                await message.reply(' всі  користувачі вже існують у бд')
+        else:
+            await message.reply(' введи айді')
+
 
 
 @dp.message_handler()

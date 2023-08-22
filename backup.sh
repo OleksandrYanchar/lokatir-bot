acd5659a410b533e79844bb5844ea008153a3072
Yurii <yuriydorosh2005@gmail.com>
2023-08-22 14:12:54 +0300

    DELETE: backup.sh and left log_backup.sh

--- backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install rsync(VPS AND YOUR PC)!!!
-# sudo apt/pacman install/-S rsync
-rsync -avz -e "ssh -p 22" user@your_vps_ip:/path/to/results.txt /local/path/on/your/pc/
-
-
-
-
-

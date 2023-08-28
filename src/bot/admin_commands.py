02bc7962359634eabb904e5e3acd57bf2e9a9138
OleksandrYanchar <oleksandryancharr@gmail.com>
2023-08-28 09:01:22 +0300

    FIX:tracking bug and returnd addUsers command

--- src/bot/admin_commands.py
+++ src/bot/admin_commands.py
@@ -31,6 +31,24 @@ async def get_ids(message: types.Message):
     await bot.send_message(message.chat.id, f"{users} ")
 
 
+@dp.message_handler(commands=['addUser'])
+async def add_user(message: types.Message):
+    if message.from_user.id in admins_ID:
+        if len(message.text.split()) > 1:
+            user_ids = [id.strip() for id in message.text.split()[1:] if id.strip().isdigit()]
+            added_users = 0
+            for user_id in user_ids:
+                if not users_db.user_exists(user_id):
+                    users_db.add_user(user_id)
+                    added_users += 1
+            if added_users > 0:
+                await message.reply(f'додано {added_users} користувачів до бд')
+            else:
+                await message.reply(' всі  користувачі вже існують у бд')
+        else:
+            await message.reply(' введи айді')
+
+
 @dp.message_handler(commands=['sendAlert'])
 async def send_message(message: types.Message):
     if message.from_user.id in admins_ID:
@@ -126,8 +144,8 @@ async def sned_tracks(message: types.Message):
 @dp.message_handler()
 #monitor spesific users messages and reply to them by sending random picture, ID is defined in chupa_id
 async def chupa(message: types.Message):
-    photo_files = [file for file in os.listdir(photos_directory) if file.endswith('.jpg')]
-    if str(message.from_user.id) in chupa_id and tracking_enabled is True:
+    if str(message.from_user.id) in str(chupa_id) and tracking_enabled:
+        photo_files = [file for file in os.listdir(photos_directory) if file.endswith('.jpg')]
         random_photo = random.choice(photo_files)
         with open(os.path.join(photos_directory, random_photo), 'rb') as photo:
             await message.reply_photo(photo)

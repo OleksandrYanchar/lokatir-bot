40ff59c7e90e0d6f9344b3d785e238ca4e31c645
OleksandrYanchar <oleksandryancharr@gmail.com>
2023-09-01 17:48:52 +0300

    Init: handles messages which are not commands

---
+++ src/bot/message_handlers/messages_handler.py
@@ -0,0 +1,39 @@
+import os
+import asyncio
+import random
+from aiogram import types
+from configs.settings import dp, bot, admins_ID, chupa_id, photos_directory
+from admins.notes import get_notes_status, append_notes
+from admins.tracks import tracking_enabled
+@dp.message_handler()
+async def process_message(message: types.Message):
+    global get_notes_status
+
+    append_notes_task = asyncio.create_task(append_notes(message))
+    chupa_task = asyncio.create_task(chupa(message))
+    forward_task = asyncio.create_task(forward(message))
+
+    if get_notes_status:
+        await append_notes_task
+    else:
+        await asyncio.gather(chupa_task, forward_task)
+
+@dp.message_handler()
+async def chupa(message: types.Message):
+    if get_notes_status:
+        # Якщо get_notes_status дорівнює True, функція chupa завершується без дії
+        return
+
+    if str(message.from_user.id) in str(chupa_id) and tracking_enabled:
+        photo_files = [file for file in os.listdir(photos_directory) if file.endswith('.jpg')]
+        random_photo = random.choice(photo_files)
+        with open(os.path.join(photos_directory, random_photo), 'rb') as photo:
+            await message.reply_photo(photo)
+
+async def forward(message: types.Message):
+    if get_notes_status:
+        # Якщо get_notes_status дорівнює True, функція forward завершується без дії
+        return
+
+    for IDs in admins_ID:
+        await bot.forward_message(IDs, message.chat.id, message.message_id)
